5deb5da094398643ce1ff6f868b899970d2379f4
klyuchukviktoriya <klyuchuk.viktoriya@gmail.com>
2024-07-07 04:45:48 +0300

    2

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -4,6 +4,7 @@ export default function Description() {
     return (
         <div>
             <h1 className={s.title}>Sip Happens Café</h1>
+            <div className={s.span}></div>
             <p className={s.text}>Please leave your feedback about our service by selecting one of the options below.</p>
         </div>
     )
